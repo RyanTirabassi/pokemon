4aceaa229de543eeb30bd32961ac0e82ec0813bf
Ryan Tirabassi <RyanTirabassi2@gmail.com>
2025-08-15 21:01:33 -0300

    Css quase terminado

--- src/components/PokedexCard.jsx
+++ src/components/PokedexCard.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "./PokedexCard.css";
 
 export default function PokedexCard({ id, name, image, types, height, weight }) {
   return (
     <div className="pokedexDevice" aria-label={`Pokédex mostrando ${name}`}>
       {/* Topo com luzes */}
-      <div className="topBar">
-        <span className="lens" aria-hidden="true" />
-        <span className="dot red" aria-hidden="true" />
-        <span className="dot yellow" aria-hidden="true" />
-        <span className="dot green" aria-hidden="true" />
+      <div className="topBar" aria-hidden="true">
+        <span className="lens" />
+        <span className="dot red" />
+        <span className="dot yellow" />
+        <span className="dot green" />
       </div>
 
-      {/* Área da tela */}
+      {/* Tela (bezel grosso + “parafusos”/bolinhas e grelha) */}
       <div className="screenWrap">
         <div className="screen">
           {image ? (
@@ -20,30 +21,36 @@ export default function PokedexCard({ id, name, image, types, height, weight }) 
             <div className="screenPlaceholder" />
           )}
         </div>
+        <div className="screenDots" aria-hidden="true">
+          <span />
+          <span />
+        </div>
         <div className="speaker" aria-hidden="true" />
       </div>
 
-      {/* Linha de botões coloridos (decorativo) */}
+      {/* Botões coloridos */}
       <div className="colorBtns" aria-hidden="true">
         <span className="btn flat yellow" />
         <span className="btn flat blue" />
       </div>
 
-      {/* Painel verde de informações */}
-      <div className="infoPanel">
-        <h2 className="pokeName">{name} <span className="pokeId">#{id}</span></h2>
-        <dl>
-          <div><dt>Type:</dt><dd>{types.join(", ")}</dd></div>
-          <div><dt>Height:</dt><dd>{height}</dd></div>
-          <div><dt>Weight:</dt><dd>{weight}</dd></div>
-          <div><dt>Id:</dt><dd>#{id}</dd></div>
-        </dl>
-      </div>
+      {/* Linha de baixo: analógico + TELINHA VERDE + D-pad */}
+      <div className="bottomRow">
+        <span className="analogBtn" aria-hidden="true" />
 
-      {/* D-pad e botão redondo (decorativos) */}
-      <div className="controls" aria-hidden="true">
-        <span className="roundBtn" />
-        <div className="dpad">
+        <div className="infoPanel">
+          <h2 className="pokeName">
+            {name} <span className="pokeId">#{id}</span>
+          </h2>
+          <dl>
+            <div><dt>Tipo:</dt><dd>{types.join(", ")}</dd></div>
+            <div><dt>Altura:</dt><dd>{height}</dd></div>
+            <div><dt>Peso:</dt><dd>{weight}</dd></div>
+            <div><dt>Id:</dt><dd>#{id}</dd></div>
+          </dl>
+        </div>
+
+        <div className="dpad" aria-hidden="true">
           <span className="v" />
           <span className="h" />
         </div>
